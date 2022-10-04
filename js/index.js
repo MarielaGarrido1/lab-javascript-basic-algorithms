bc150f77e3fbc77be3ec71a2382e24df1f070a27
Mariela Garrido <luz.mathphys@gmail.com>
2022-10-04 00:44:15 -0500

    it 1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,19 @@
 // Iteration 1: Names and Input
-//
+
+function iteration1 (name1, name2){ 
+    const hacker1 = name1
+    const driver = name2
+    return{
+        hacker1: `The driver's name is ${hacker1}`,
+        driver: `The navigators name is ${driver}`
+    }
+}
+
+const results = iteration1("Elisa", "Polar")
+console.log(results.hacker1) 
+console.log(results.driver)
+
+
 // Iteration 2: Conditionals
 
 
